579a6b68a74e80b15e63aecfc33cafa1c74b4608
ImranParthib <imranparthib13@gmail.com>
2024-12-02 00:08:52 +0600

    🚀 Implement notification page with Tailwind CSS and React components

--- notification-page/src/App.jsx
+++ notification-page/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-2/3 m-auto  bg-slate-100 my-10 p-6">
+      <div className="flex justify-between">
+        <span className="font-bold">
+          Notifications{" "}
+          <strong className="bg-blue-700 rounded-sm px-2 text-white">
+            {4}
+          </strong>
+        </span>
+        <span className="text-gray-600">Mark all as read</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
